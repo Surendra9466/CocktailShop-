9f86052b3b65af612ec9eca6cb53fe8deced9450
Surendra <ssingh@forbestech.com>
2022-01-04 17:05:25 +0530

    added search feature

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
+export default function SearchForm({ setSearchTerm }) {
+  const searchValue = useRef("");
+  const searchCocktail = () => { 
+    console.log(searchValue.current.value);
+    setSearchTerm(searchValue.current.value);
+  }
+  // const searchCocktail = (e) => { }
+
+  return <section className="section">
+    <h2 className="section-title">search cocktails</h2>
+    <form className="form search-form" >
+      <div className="form-control">
+        <label htmlFor="name">
+          search your favorite cocktail
+        </label>
+        <input type="text" name="name" id="name" onChange={searchCocktail} ref={searchValue}/>
+      </div>
+    </form>
+  </section>;
 }
